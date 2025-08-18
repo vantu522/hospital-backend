f7be28e7cf614338a292e3133f54ddecc880d78b
tudgvn23 <tuduongvan2305@gmail.com>
2025-08-18 15:40:12 +0700

    check

--- src/routes/customer.router.js
+++ src/routes/customer.router.js
@@ -3,12 +3,12 @@ import customerController from '../app/controllers/customer.controller.js';
 import { validateCustomerRegister, validateCustomerLogin } from '../app/middlewares/customer.validator.js';
 import { validate } from '../app/middlewares/validate.js';
 
-const router = express.Router();
+const customerRouter = express.Router();
 
 // Đăng ký customer
-router.post('/register', validateCustomerRegister, validate, customerController.register);
+customerRouter.post('/register', validateCustomerRegister, validate, customerController.register);
 
 // Đăng nhập customer
-router.post('/login', validateCustomerLogin, validate, customerController.login);
+customerRouter.post('/login', validateCustomerLogin, validate, customerController.login);
 
-export default router;
+export default customerRouter;
