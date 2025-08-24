e88f96e0d5079aed3be3f2d9c1307349eaeac01b
tudgvn23 <tuduongvan2305@gmail.com>
2025-08-24 11:41:47 +0700

    merger

--- src/routes/doctor.router.js
+++ src/routes/doctor.router.js
@@ -1,21 +1,33 @@
-import { Router } from 'express';
-import * as doctorController from '../app/controllers/doctor.controller.js';
-import upload from '../app/middlewares/upload.js';
-import { requireAdminOrSuperadmin } from '../app/middlewares/auth.js';
+import { Router } from "express";
+import * as doctorController from "../app/controllers/doctor.controller.js";
+import upload from "../app/middlewares/upload.js";
+import { requireAdminOrSuperadmin } from "../app/middlewares/auth.js";
 
 const doctorRouter = Router();
 
-doctorRouter.post('/', requireAdminOrSuperadmin, upload.fields([
-    { name: 'avatar', maxCount: 1 }
-]), doctorController.createDoctor);
-doctorRouter.get('/', doctorController.getAllDoctors);
-doctorRouter.get('/specialty/:specialtyId', doctorController.getDoctorsBySpecialty);
-doctorRouter.get('/slug/:slug', doctorController.getDoctorBySlug);
-doctorRouter.put('/:id', requireAdminOrSuperadmin, upload.fields([
-    { name: 'avatar', maxCount: 1 }
-]), doctorController.updateDoctor);
-doctorRouter.delete('/:id', requireAdminOrSuperadmin, doctorController.deleteDoctor);
+doctorRouter.post(
+  "/",
+  requireAdminOrSuperadmin,
+  upload.fields([{ name: "avatar", maxCount: 1 }]),
+  doctorController.createDoctor
+);
+doctorRouter.get("/", doctorController.getAllDoctors);
+doctorRouter.get(
+  "/specialty/:specialtyId",
+  doctorController.getDoctorsBySpecialty
+);
+doctorRouter.get("/slug/:slug", doctorController.getDoctorBySlug);
+doctorRouter.put(
+  "/:id",
+  requireAdminOrSuperadmin,
+  upload.fields([{ name: "avatar", maxCount: 1 }]),
+  doctorController.updateDoctor
+);
+doctorRouter.delete(
+  "/:id",
+  requireAdminOrSuperadmin,
+  doctorController.deleteDoctor
+);
 doctorRouter.get("/five-doctors", doctorController.getFiveRandomDoctors);
-
 
 export default doctorRouter;
