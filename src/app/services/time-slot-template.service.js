aac94042dd8af3f812f56bd57897d11850b10fb8
anhle1106 <leanh9331@gmail.com>
2025-09-29 20:25:19 +0700

    fix khung gio kham

--- src/app/services/time-slot-template.service.js
+++ src/app/services/time-slot-template.service.js
@@ -6,7 +6,14 @@ class TimeSlotTemplateService {
   }
 
   async getTemplates() {
-    return await TimeSlotTemplateRepository.findAll();
+    const templates = await TimeSlotTemplateRepository.findAll();
+    // Sắp xếp theo trường time tăng dần (giả sử time là chuỗi 'HH:mm')
+    templates.sort((a, b) => {
+      const [ah, am] = a.time.split(':').map(Number);
+      const [bh, bm] = b.time.split(':').map(Number);
+      return ah !== bh ? ah - bh : am - bm;
+    });
+    return templates;
   }
 
   async deleteTemplate(id) {
