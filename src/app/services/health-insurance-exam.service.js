e6d5d4d5f577de15da62ad2c188f51cc136985c5
anhle1106 <leanh9331@gmail.com>
2025-10-06 00:32:20 +0700

    fix loi cccd khi kham dv

--- src/app/services/health-insurance-exam.service.js
+++ src/app/services/health-insurance-exam.service.js
@@ -697,6 +697,7 @@ class HealthInsuranceExamService {
         IdBenhVien: process.env.ID_BENHVIEN_HIS,
         IdKhoaKham: exam.IdKhoaKham,
         IsDonTiepCCCD: !!exam.CCCD,
+        CMND: exam.CCCD,
         CMNDNoiCap: process.env.NOI_CAP_CCCD_HIS,
         CMNDNgayCap: this.formatDisplayDateTime(exam.CMNDNgayCap, false),
         IsMSTCaNhan: true,
@@ -727,7 +728,6 @@ class HealthInsuranceExamService {
           IsBHYT: !!exam.dmBHYT,
           IsDungTuyen: !!exam.dmBHYT,
           SoBHYT: exam.dmBHYT ? exam.dmBHYT.SoBHYT : exam.SoBHYT,
-          CMND: exam.CCCD
         }
         : basePayload;
 
